ffa7565bb97a22071f64f9a67549f894bf2cf54e
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-29 16:24:52 -0300

    feat: Ensure logo will have the right size

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -4,8 +4,21 @@ import styled, { css } from "styled-components";
 
 import { type LogoProps } from ".";
 
+const wrapperModifiers = {
+	normal: () => css`
+		width: 11rem;
+		height: 3.3rem;
+	`,
+	large: () => css`
+		width: 20rem;
+		height: 5.9rem;
+	`,
+};
+
 export const Wrapper = styled.div<LogoProps>`
-	${({ theme, color }) => css`
+	${({ theme, color, size }) => css`
 		color: ${theme.colors[color!]};
+
+		${wrapperModifiers[size!]}
 	`}
 `;
