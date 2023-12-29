625534540a6119b1912449675fb23474cabd84ce
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-29 17:40:17 -0300

    test: Ensure logo will have text removed on mobile

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-const developmentOrTestOnly = process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	compiler: {
-		styledComponents: {
-			displayName: developmentOrTestOnly,
-		}
+		styledComponents: true,
 	},
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,24 +1,47 @@
 "use-client";
 
 import styled, { css } from "styled-components";
-
-import { type LogoProps } from ".";
+import media from "styled-media-query";
 
 const wrapperModifiers = {
 	normal: () => css`
 		width: 11rem;
 		height: 3.3rem;
 	`,
+
 	large: () => css`
 		width: 20rem;
 		height: 5.9rem;
 	`,
+
+	hideOnMobile: () => css`
+		${media.lessThan("medium")`
+			width: 5.8rem;
+			height: 4.5rem;
+
+			svg {
+				height: 4.5rem;
+				pointer-events: none;
+			}
+
+			.text {
+				display: none;
+			}
+		`}
+	`,
 };
 
-export const Wrapper = styled.div<LogoProps>`
-	${({ theme, color, size }) => css`
-		color: ${theme.colors[color!]};
+type WrapperProps = {
+	$color?: "white" | "black";
+	$size?: "normal" | "large";
+	$hideOnMobile?: boolean;
+};
 
-		${wrapperModifiers[size!]}
+export const Wrapper = styled.div<WrapperProps>`
+	${({ theme, $color, $size, $hideOnMobile }) => css`
+		color: ${theme.colors[$color!]};
+
+		${!!$size && wrapperModifiers[$size]}
+		${!!$hideOnMobile && wrapperModifiers.hideOnMobile}
 	`}
 `;
