41f78e50be022e4c26dd26e80b5b64038d5405b5
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-31 10:16:09 -0300

    feat: add animation for MenuFull

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -58,6 +58,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
 		right: 0;
 		height: 100vh;
 		overflow: hidden;
+		transition: opacity 0.3s ease-in-out;
 		pointer-events: ${$isOpen ? "all" : "none"};
 		opacity: ${$isOpen ? 1 : 0};
 
@@ -76,6 +77,8 @@ export const MenuFull = styled.nav<MenuFullProps>`
 			font-weight: ${theme.font.bold};
 			font-size: ${theme.font.sizes.xlarge};
 			margin-bottom: ${theme.spacings.small};
+			transform: ${$isOpen ? "translateY(0)" : "translateY(3rem)"};
+			transition: transform 0.3s ease-in-out;
 		}
 
 		${MenuNav} {
@@ -84,6 +87,11 @@ export const MenuFull = styled.nav<MenuFullProps>`
 			justify-content: center;
 			flex: 1;
 			flex-direction: column;
+		}
+
+		${RegisterBox} {
+			transform: ${$isOpen ? "translateY(0)" : "translateY(3rem)"};
+			transition: transform 0.3s ease-in-out;
 		}
 	`}
 `;
