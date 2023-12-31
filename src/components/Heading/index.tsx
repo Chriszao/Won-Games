f7e459b8303e111bfe30e1cab554ba3fc7e83a72
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-31 11:45:51 -0300

    refactor: make heading receive a prop to change size and lineColor

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,11 +2,15 @@ import { type ReactNode } from "react";
 
 import * as S from "./styles";
 
+export type LineColors = "primary" | "secondary";
+
 export type HeadingProps = {
 	children: ReactNode;
 	color?: "white" | "black";
 	lineLeft?: boolean;
 	lineBottom?: boolean;
+	size?: "small" | "medium";
+	lineColor?: LineColors;
 };
 
 export function Heading({
@@ -14,9 +18,17 @@ export function Heading({
 	color = "white",
 	lineLeft = false,
 	lineBottom = false,
+	size = "medium",
+	lineColor = "primary",
 }: HeadingProps) {
 	return (
-		<S.Wrapper $lineBottom={lineBottom} $lineLeft={lineLeft} $color={color}>
+		<S.Wrapper
+			$lineColor={lineColor}
+			$size={size}
+			$lineBottom={lineBottom}
+			$lineLeft={lineLeft}
+			$color={color}
+		>
 			{children}
 		</S.Wrapper>
 	);
