fbb467e5e22d25117d705dc8b011514750a5db2f
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-31 09:52:58 -0300

    feat: Add logic to open an close menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,18 +1,22 @@
 import {
 	ShoppingCart as ShoppingCartIcon,
 	Search as SearchIcon,
+	Close as CloseIcon,
 } from "@styled-icons/material-outlined";
 import { Menu2 as MenuIcon } from "@styled-icons/remix-fill";
+import { useState } from "react";
 
 import { Logo } from "~/components/Logo";
 
 import * as S from "./styles";
 
 export function Menu() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<S.Wrapper>
 			<S.IconWrapper>
-				<MenuIcon aria-label="Open Menu" />
+				<MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
 			</S.IconWrapper>
 
 			<S.LogoWrapper>
@@ -28,6 +32,10 @@ export function Menu() {
 					<ShoppingCartIcon aria-label="Open Shopping Cart" />
 				</S.IconWrapper>
 			</S.MenuGroup>
+
+			<S.MenuFull $isOpen={isOpen} aria-hidden={!isOpen}>
+				<CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+			</S.MenuFull>
 		</S.Wrapper>
 	);
 }
